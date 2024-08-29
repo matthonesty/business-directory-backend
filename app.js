c357d8a0c1db3f0b525e9cd3a163c30ab79c7e07
Fitsumhelina <permin1501424@gmail.com>
2024-08-29 23:26:19 +0300

    port file moved to .env file

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
