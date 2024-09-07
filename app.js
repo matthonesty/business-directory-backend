27ec6bc9d34194dd18e10adc887ccbbd1b7dc6b6
Yabe12 <yeabsirabehailu92@gmail.com>
2024-09-07 07:12:50 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const businessRoutes=require("./routes/businessRoutes")
-const authRoutes = require("./routes/authRoutes");
+
 const userRoutes = require("./routes/userRoutes");
 
 // Middleware
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // Use routes
 app.use("/api/users", userRoutes);
-app.use("/api/auth", authRoutes);
+
 app.use('/api/businesses', businessRoutes);
 
 // Error handling middleware
