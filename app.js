87f5964c6857cb0792d7428d944cc0498db1eee8
Fitsumhelina <permin1501424@gmail.com>
2024-09-19 18:16:49 +0300

    static page hosted in server

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const businessRoutes=require("./routes/businessRoutes")
 const userRoutes = require("./routes/userRoutes");
 
+app.use('/uploads', express.static('uploads'));
+
 // Middleware
 app.use(express.json());
 
