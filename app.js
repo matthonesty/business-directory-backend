2639053c8f1b718d4f15db0c46fd7ff46a1d3941
Fitsumhelina <permin1501424@gmail.com>
2024-09-19 18:19:41 +0300

    file limit added 10 mb

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const businessRoutes=require("./routes/businessRoutes")
 const userRoutes = require("./routes/userRoutes");
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.json({ limit: '10mb' }));
+
 
 app.use('/uploads', express.static('uploads'));
 
